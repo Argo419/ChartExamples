ea7086cc971d1d8c7d351e3e07d6cb32bdded5cd
Argo419 <argyn-muratuly@mail.ru>
2024-08-10 23:51:12 +0500

    dobav

--- ChartExamples.py
+++ ChartExamples.py
@@ -2,11 +2,18 @@
 
 import tkinter as tk
 
+# Функция закрытия программы
+def do_close():
+	window.destroy()
+
 # Созданиие главного окна
 window = tk. Tk()
 window.geometry("450x450")
 window.title("Примеры построения графиков")
 
+# добавление кнопки закрытия программмы
+btnClose = tk.Button(window, text="Zakryt", font = ('helvetica', 10, 'bold'),command=do_close)
+btnClose.place(x=330, y=400, width=90, height=30)
 
 # Запуск цикла mainloop
 window.mainloop()
